fb87dd666c8273a3bae5a775cbd03d58a475d084
u-ulim <uiissoop@gmail.com>
2025-03-22 23:14:22 +0900

    tailwindCSS

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="bg-blue-500 p-12">
+      <h1 className="text-5xl font-bold text-white">
+        This Tailwind 4.0 version
+      </h1>
+    </div>
+  );
+}
