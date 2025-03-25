5fea66f9cb268d5d759bada6f3d2f6288b19f443
u-ulim <uiissoop@gmail.com>
2025-03-25 18:23:17 +0900

    0325

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import Header from "@/components/Header";
 import { Footer } from "@/components/Footer";
 
+import { SupabaseProvider } from "./provider/SupabaseProvider";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -29,9 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
-        {/* <Header /> */}
-        {children}
-        <Footer />
+        <SupabaseProvider>{children}</SupabaseProvider>
       </body>
     </html>
   );
