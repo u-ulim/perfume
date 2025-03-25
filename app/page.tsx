e0986bdbcb29c936ac285ff849922798adee0c60
u-ulim <uiissoop@gmail.com>
2025-03-25 16:34:26 +0900

    basic login

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import { BestSeller } from "@/components/home/bestseller/BestSeller";
 import MainSlider from "@/components/home/slide/MainSlider";
 
+import { logout } from "./logout/actions";
+
 export default function Home() {
   return (
     <div className="flex flex-col gap-[200px] mb-[200px]">
       <MainSlider />
+      <form action={logout}>
+        <button type="submit">logout</button>
+      </form>
       <BestSeller />
     </div>
   );
