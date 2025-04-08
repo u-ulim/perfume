a216b5b48646152a3349f9260cbd93dfd4a595cf
u-ulim <uiissoop@gmail.com>
2025-04-08 22:36:18 +0900

    login ui update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -19,46 +19,58 @@ export default function LoginPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4 py-16">
       <form className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
-        <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
+        <h2 className="text-2xl font-bold mb-14 text-center">향기조아</h2>
 
-        <div className="mb-4">
-          <label htmlFor="email" className="block text-gray-700 mb-2">
-            Email:
-          </label>
+        <div className="relative mb-4">
           <input
             id="email"
             name="email"
             type="email"
             required
-            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder=" "
+            className="peer block w-full px-4 py-2 border border-gray-300 rounded-md bg-white focus:outline-none"
           />
-        </div>
-
-        <div className="mb-6">
-          <label htmlFor="password" className="block text-gray-700 mb-2">
-            Password:
+          <label
+            htmlFor="email"
+            className="absolute left-4 top-2 z-10 bg-white px-1 text-gray-500 transition-all duration-200 ease-in-out
+               peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-sm
+               peer-focus:-top-[8px] peer-focus:text-xs"
+          >
+            사용자이름, 이메일주소
           </label>
+        </div>
+        <div className="relative mb-[40px]">
           <input
             id="password"
             name="password"
             type="password"
             required
-            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder=" "
+            className="peer block w-full px-4 py-2 border border-gray-300 rounded-md bg-white focus:outline-none"
           />
+          <label
+            htmlFor="password"
+            className="absolute left-4 top-2 z-10 bg-white px-1 text-gray-500 transition-all duration-200 ease-in-out
+                peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-sm
+               peer-focus:-top-[8px] peer-focus:text-xs"
+          >
+            비밀번호
+          </label>
         </div>
 
-        <div className="flex justify-between">
+        <div>
           <button
             formAction={login}
-            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+            className="block mx-auto hover:text-blue-600 text-gray-400 text12  py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
           >
-            Log in
+            로그인 하기
           </button>
+
           <button
             formAction={signup}
-            className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-green-400"
+            className="block mx-auto hover:text-green-600 text-gray-400 text12  py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-green-400"
           >
-            Sign up
+            회원가입 하기
           </button>
         </div>
       </form>
